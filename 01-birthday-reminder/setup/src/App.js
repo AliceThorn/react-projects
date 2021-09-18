eac8d72264b931c70a2d4079168f44ec292e8a79
AliceThorn <aliciathornthwaite@gmail.com>
2021-09-18 11:10:37 +0100

    React Project Commit
    React Project Commit

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,21 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+
+
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data)
+
+  return (
+  <main>
+    <section classname='container'>
+      <h3> {people.length} Birthdays Today </h3>
+      <List people={people}/>
+      <button onClick={() => setPeople([])}>Clear All</button>
+      <button onClick={(<List people={people}/>)}>Todays Reminders</button>
+    </section>
+  </main>
+  );
 }
 
 export default App;
